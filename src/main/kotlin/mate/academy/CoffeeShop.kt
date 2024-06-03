1d8094846e3fec46f5aee3ab859bebd565f88c25
Nonzeq <bgdn.kobylchak@gmail.com>
2024-06-03 14:21:15 +0300

    added

--- src/main/kotlin/mate/academy/CoffeeShop.kt
+++ src/main/kotlin/mate/academy/CoffeeShop.kt
@@ -1,11 +1,12 @@
 package mate.academy
 
-// A class representing a CoffeeShop
 class CoffeeShop {
-
-    // Method to sell coffee to a client
     fun sellCoffee(clientName: String, coffeeType: String): CoffeeCup {
-        // Create and initialize a CoffeeCup, then apply the serve action
-
+        return CoffeeCup().apply {
+            this.coffeeType = coffeeType
+            this.clientName = clientName
+            this.isServed = true
+            serveCoffee()
+        }
     }
 }
