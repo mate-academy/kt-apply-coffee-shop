d0fa98668c7b238ec98914864a82372dce7ea53c
Alex Zavodov <96802636+lexzane@users.noreply.github.com>
2024-07-21 11:02:12 +0300

    feat [kotlin]: implement simple coffee shop service

--- src/main/kotlin/mate/academy/CoffeeShop.kt
+++ src/main/kotlin/mate/academy/CoffeeShop.kt
@@ -3,9 +3,10 @@ package mate.academy
 // A class representing a CoffeeShop
 class CoffeeShop {
 
-    // Method to sell coffee to a client
-    fun sellCoffee(clientName: String, coffeeType: String): CoffeeCup {
-        // Create and initialize a CoffeeCup, then apply the serve action
-
+    fun sellCoffee(clientName: String, coffeeType: String): CoffeeCup = CoffeeCup().apply {
+        this.clientName = clientName
+        this.coffeeType = coffeeType
+        isServed = true
+        serveCoffee()
     }
 }
