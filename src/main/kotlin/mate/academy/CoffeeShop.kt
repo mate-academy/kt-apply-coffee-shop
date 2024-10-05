aaad9f6c45afebaee470aeb53f250a7556a1f9c1
Rostyslav Kuzmych <rostislavkuzmic401@gmail.com>
2024-10-05 12:09:52 +0300

    Implemented the feature

--- src/main/kotlin/mate/academy/CoffeeShop.kt
+++ src/main/kotlin/mate/academy/CoffeeShop.kt
@@ -3,9 +3,12 @@ package mate.academy
 // A class representing a CoffeeShop
 class CoffeeShop {
 
-    // Method to sell coffee to a client
     fun sellCoffee(clientName: String, coffeeType: String): CoffeeCup {
-        // Create and initialize a CoffeeCup, then apply the serve action
-
+        return CoffeeCup().apply {
+            isServed = true
+            this.clientName = clientName
+            this.coffeeType = coffeeType
+            serveCoffee()
+        }
     }
 }
